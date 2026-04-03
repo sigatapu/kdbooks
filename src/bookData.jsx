d239dcc91848cb446fc1a628e2716e3f4e368a54
Sigatapu <dattakrishna282@gmail.com>
2026-04-03 14:10:30 +0530

    added some more books

--- src/bookData.jsx
+++ src/bookData.jsx
@@ -1,106 +1,122 @@
 export const bookData = [
-  // 1 (Completed) - Atomic Habits
+  // 1 (Completed) - Building the Discipline
   {
     id: 1,
-    image: "https://m.media-amazon.com/images/I/81F90H7hnML._AC_UF1000,1000_QL80_.jpg",
     name: "Atomic Habits",
     author: "James Clear",
+    image: "https://m.media-amazon.com/images/I/81F90H7hnML._AC_UF1000,1000_QL80_.jpg",
     read: "/Atomic habits.pdf"
   },
-  // 2 - The 48 Laws of Power
+  // 2 - The Stoic Shield: Emotional Detachment
   {
     id: 2,
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbA4NBdsNvLrNkABlMQPqUceL7AUcFvK0U9Q&s",
-    name: "The 48 Laws of Power",
-    author: "Robert Greene",
-    read: "/THE 48 LAWS OF POWER.pdf"
-  },
-  // 3 - Meditations
-  {
-    id: 3,
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWHoAr9UNSY4lo3IhDZECE_0uExZn6b7v-QQ&s",
     name: "Meditations",
     author: "Marcus Aurelius",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWHoAr9UNSY4lo3IhDZECE_0uExZn6b7v-QQ&s",
     read: "/meditations.pdf"
   },
-  // 4 - Thinking, Fast and Slow
+  // 3 - Focus & Mental Endurance: The "White Room" work ethic
+  {
+    id: 3,
+    name: "Deep Work",
+    author: "Cal Newport",
+    image: "https://m.media-amazon.com/images/I/61zt25yYrCL._SY466_.jpg",
+    read: "/Deep Work.pdf"
+  },
+  // 4 - Rationality: Removing Cognitive Biases
   {
     id: 4,
-    image: "https://m.media-amazon.com/images/I/71f6DceqZAL._AC_UF1000,1000_QL80_.jpg",
     name: "Thinking, Fast and Slow",
     author: "Daniel Kahneman",
+    image: "https://m.media-amazon.com/images/I/71f6DceqZAL._AC_UF1000,1000_QL80_.jpg",
     read: "/thinking fast and slow.pdf"
   },
-  // 5 - How to Win Friends and Influence People
+  // 5 - Cold Prioritization: Ignoring Social Validation
   {
     id: 5,
-    image: "https://m.media-amazon.com/images/I/71vK0WVQ4rL._AC_UF1000,1000_QL80_.jpg",
-    name: "How to Win Friends and Influence People",
-    author: "Dale Carnegie",
-    read: "/How to Win Friends and Influence People - Dale Carnegie.pdf"
-  },
-  // 6 - The Subtle Art of Not Giving a F*ck
-  {
-    id: 6,
-    image: "https://m.media-amazon.com/images/I/71QKQ9mwV7L.jpg",
     name: "The Subtle Art of Not Giving a F*ck",
     author: "Mark Manson",
+    image: "https://m.media-amazon.com/images/I/71QKQ9mwV7L.jpg",
     read: "/The Subtle Art of Not Giving a Fck.pdf"
   },
-  // 8 - The Prince
+  // 6 - Cold Reading: Classifying Personalities
   {
-    id: 8,
-    image: "https://m.media-amazon.com/images/I/4117w9ptZnL._AC_UF1000,1000_QL80_.jpg",
-    name: "The Prince",
-    author: "Niccolò Machiavelli",
-    read: "/The prince.pdf"
-  },
-  // 9 - The Art of War
-  {
-    id: 9,
-    image: "https://m.media-amazon.com/images/I/811ez0yerlL._AC_UF1000,1000_QL80_.jpg",
-    name: "The Art of War",
-    author: "Sun Tzu",
-    read: "/The_Art_Of_War.pdf"
-  },
-  // 10 - Mastery
-  {
-    id: 10,
-    image: "https://m.media-amazon.com/images/I/71Z9zJnT4vL.jpg",
-    name: "Mastery",
-    author: "Robert Greene",
-    read: "/Mastery.pdf"
-  },
-  // 11 - Influence: The Psychology of Persuasion
-  {
-    id: 11,
-    image: "https://m.media-amazon.com/images/I/61wwAomEffL.jpg",
-    name: "Influence: The Psychology of Persuasion",
-    author: "Robert Cialdini",
-    read: "/The Psychology of Persuasion.pdf"
-  },
-  // 12 - Atlas Shrugged
-  {
-    id: 12,
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-o5kLdMv1IAs1_ZcMRFc-Pj2QkMnK5bI9Jw&s",
-    name: "Atlas Shrugged",
-    author: "Ayn Rand",
-    read: "/Ayn Rand- Atlas Shrugged.pdf"
-  },
-  // 13 - Surrounded by Idiots
-  {
-    id: 13,
-    image: "https://listeningbooks.s3.eu-west-2.amazonaws.com/public/books/500x0/16193.jpg",
+    id: 6,
     name: "Surrounded by Idiots",
     author: "Thomas Erikson",
+    image: "https://listeningbooks.s3.eu-west-2.amazonaws.com/public/books/500x0/16193.jpg",
     read: "/Surrounded by Idiots.pdf"
   },
+  // 7 - Influence: Understanding Why People Act
   {
-    id:14,
-    image:"https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1581527774i/41881472.jpg",
-    name : "The Psychology of money",
-    author:"Morgan Housel",
-    read : "/The-Psychology-of-Money.pdf"
-
+    id: 7,
+    name: "Influence: The Psychology of Persuasion",
+    author: "Robert Cialdini",
+    image: "https://m.media-amazon.com/images/I/61wwAomEffL.jpg",
+    read: "/The Psychology of Persuasion.pdf"
+  },
+  // 8 - Mastering Social Optics
+  {
+    id: 8,
+    name: "How to Win Friends and Influence People",
+    author: "Dale Carnegie",
+    image: "https://m.media-amazon.com/images/I/71vK0WVQ4rL._AC_UF1000,1000_QL80_.jpg",
+    read: "/How to Win Friends and Influence People - Dale Carnegie.pdf"
+  },
+  // 9 - Pure Strategy: Thinking in Terms of Win/Loss
+  {
+    id: 9,
+    name: "The Art of War",
+    author: "Sun Tzu",
+    image: "https://m.media-amazon.com/images/I/811ez0yerlL._AC_UF1000,1000_QL80_.jpg",
+    read: "/The_Art_Of_War.pdf"
+  },
+  // 10 - The Mechanisms of Power
+  {
+    id: 10,
+    name: "The 48 Laws of Power",
+    author: "Robert Greene",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbA4NBdsNvLrNkABlMQPqUceL7AUcFvK0U9Q&s",
+    read: "/THE 48 LAWS OF POWER.pdf"
+  },
+  // 11 - Political Pragmatism
+  {
+    id: 11,
+    name: "The Prince",
+    author: "Niccolò Machiavelli",
+    image: "https://m.media-amazon.com/images/I/4117w9ptZnL._AC_UF1000,1000_QL80_.jpg",
+    read: "/The prince.pdf"
+  },
+  // 12 - Skill Acquisition: Achieving "Genius" levels
+  {
+    id: 12,
+    name: "Mastery",
+    author: "Robert Greene",
+    image: "https://m.media-amazon.com/images/I/71Z9zJnT4vL.jpg",
+    read: "/Mastery.pdf"
+  },
+  // 13 - Charisma & Psychological Charm
+  {
+    id: 13,
+    name: "The art of seduction",
+    author: "Robert Greene",
+    image: "https://m.media-amazon.com/images/I/61uFWH2V1DL._AC_UF1000,1000_QL80_.jpg",
+    read: "/the-art-of-seduction-robert-greene.pdf"
+  },
+  // 14 - Financial Logic
+  {
+    id: 14,
+    name: "The Psychology of money",
+    author: "Morgan Housel",
+    image: "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1581527774i/41881472.jpg",
+    read: "/The-Psychology-of-Money.pdf"
+  },
+  // 15 - The Philosophical Individual
+  {
+    id: 15,
+    name: "Atlas Shrugged",
+    author: "Ayn Rand",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-o5kLdMv1IAs1_ZcMRFc-Pj2QkMnK5bI9Jw&s",
+    read: "/Ayn Rand- Atlas Shrugged.pdf"
   }
 ];
